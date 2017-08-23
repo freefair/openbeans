98c308b8135b1197b1c5db82b75dfb7b4ec27d2f
Lars Grefer <github@larsgrefer.de>
2017-08-23 22:42:13 +0200

    add ConstructorProperties support

--- src/main/java/com/googlecode/openbeans/ConstructorProperties.java
+++ src/main/java/com/googlecode/openbeans/ConstructorProperties.java
@@ -1,4 +1,15 @@
 package com.googlecode.openbeans;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Target(CONSTRUCTOR)
+@Retention(RUNTIME)
 public @interface ConstructorProperties {
+    String[] value();
 }
